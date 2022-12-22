b1f656bea4947730d4417671fffa46143230aff3
Sam McClatchey <samteamender@gmail.com>
2022-12-22 16:40:47 -0600

    Added Wing item

--- src/main/java/net/chunkymonkey/fglider/FabricGlider.java
+++ src/main/java/net/chunkymonkey/fglider/FabricGlider.java
@@ -1,15 +1,16 @@
 package net.chunkymonkey.fglider;
 
+import net.chunkymonkey.fglider.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class FabricGlider implements ModInitializer {
-	public static final String FGLIDER = "fglider";
-	public static final Logger LOGGER = LoggerFactory.getLogger(FGLIDER);
+	public static final String MOD_ID = "fglider";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
