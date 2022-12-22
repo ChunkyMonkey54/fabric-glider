a9fa59cd4cf72077ca19d8ab2605e41bc777e345
Sam McClatchey <samteamender@gmail.com>
2022-12-22 17:49:01 -0600

    Added Glider item & recipe

--- src/main/java/net/chunkymonkey/fglider/item/ModItems.java
+++ src/main/java/net/chunkymonkey/fglider/item/ModItems.java
@@ -5,12 +5,15 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
     public static final Item WING = registerItem("wing",
             new Item(new FabricItemSettings().group(ItemGroup.MATERIALS)));
+    public static final Item GLIDER = registerItem("glider",
+            (Item)(new GliderItem((new Item.Settings()).maxDamage(432).group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON))));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(FabricGlider.MOD_ID, name), item);
