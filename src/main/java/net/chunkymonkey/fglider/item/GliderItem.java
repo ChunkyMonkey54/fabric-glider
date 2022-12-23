5a51a019e37bbb52e6651dfb528d075117cf0d7a
Sam McClatchey <samteamender@gmail.com>
2022-12-22 18:52:08 -0600

    Removed redundant code

--- src/main/java/net/chunkymonkey/fglider/item/GliderItem.java
+++ src/main/java/net/chunkymonkey/fglider/item/GliderItem.java
@@ -1,17 +1,6 @@
 package net.chunkymonkey.fglider.item;
 
-import net.minecraft.block.DispenserBlock;
-import net.minecraft.entity.EquipmentSlot;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
-import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.stat.Stats;
-import net.minecraft.util.Hand;
-import net.minecraft.util.TypedActionResult;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
 public class GliderItem extends ElytraItem {
 
