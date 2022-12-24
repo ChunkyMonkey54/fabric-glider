b8f3a329cf1ab42289599a0420402c1ae9757a24
Sam McClatchey <samteamender@gmail.com>
2022-12-23 21:53:56 -0600

    Added a broken texture for the Glider

--- src/main/java/net/chunkymonkey/fglider/FabricGliderClient.java
+++ src/main/java/net/chunkymonkey/fglider/FabricGliderClient.java
@@ -1,10 +1,15 @@
 package net.chunkymonkey.fglider;
 
+import net.chunkymonkey.fglider.item.GliderItem;
+import net.chunkymonkey.fglider.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
+import net.minecraft.util.Identifier;
 
 public class FabricGliderClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        ModelPredicateProviderRegistry.register(ModItems.GLIDER, new Identifier("broken"),
+                (stack, arg1, arg2, arg3) -> GliderItem.isUsable(stack) ? 0 : 1);
     }
 }
